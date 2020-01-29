e062d7de4ddad235bc8ed7747b6a3d9f673179ff
David Tran <davidttemail@gmail.com>
2020-01-29 13:33:32 -0700

    Add test for standard names

--- test/school_test.rb
+++ test/school_test.rb
@@ -53,4 +53,13 @@ class SchoolTest < Minitest::Test
 
     assert_equal true, school.hours_in_school_day > 4
   end
+
+  def test_standard_names
+    school = School.new('9:00', 7)
+    school.add_student_name('Aurora')
+    school.add_student_name('tim')
+    school.add_student_name('megan')
+
+    assert_equal ["Aurora", "Tim", "Megan"], school.standard_student_names
+  end
 end
