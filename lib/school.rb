aeb9d3934d2a4480b3e3837feb2097e42622efa3
David Tran <davidttemail@gmail.com>
2020-01-29 14:01:21 -0700

    Add 'end_time' method

--- lib/school.rb
+++ lib/school.rb
@@ -1,15 +1,20 @@
 class School
   attr_reader :start_time, :hours_in_school_day
-  attr_accessor :student_names
+  attr_accessor :student_names, :end_time
 
   def initialize(start_time, hours_in_school_day)
     @start_time = start_time
     @hours_in_school_day = hours_in_school_day
     @student_names = []
+    @end_time = end_time
   end
 
   def add_student_name(name)
     @student_names << name
+  end
+
+  def end_time
+    require "pry"; binding.pry
   end
 
   def standard_student_names
