ecf13b8362e9139c66a82571a5ed034af255648c
David Tran <davidttemail@gmail.com>
2020-01-29 13:11:53 -0700

    Add attr_reader/accessor

--- lib/school.rb
+++ lib/school.rb
@@ -1,4 +1,7 @@
 class School
+  attr_reader :start_time, :hours_in_school_day
+  attr_accessor :students
+
   def initialize(start_time, hours_in_school_day)
     @start_time = start_time
     @hours_in_school_day = hours_in_school_day
