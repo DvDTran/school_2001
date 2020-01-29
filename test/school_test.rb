6c9c9eaafb7901eb8067fba7fc80fa6c098a2c15
David Tran <davidttemail@gmail.com>
2020-01-29 13:27:33 -0700

    Add test_if_full_time

--- test/school_test.rb
+++ test/school_test.rb
@@ -11,21 +11,18 @@ class SchoolTest < Minitest::Test
   end
 
   def test_it_has_start_time
-    skip
     school = School.new('9:00', 7)
 
     assert_equal '9:00', school.start_time
   end
 
   def test_it_has_hours_in_school_day
-    skip
     school = School.new('9:00', 7)
 
     assert_equal 7, school.hours_in_school_day
   end
 
   def test_it_starts_with_no_student_names
-    skip
     school = School.new('9:00', 7)
 
     assert_equal [], school.student_names
@@ -33,7 +30,6 @@ class SchoolTest < Minitest::Test
 
   #Iteration 2 Tests:
   def test_it_can_add_student_names
-    skip
     school = School.new('9:00', 7)
 
     school.add_student_name('Aurora')
@@ -50,5 +46,11 @@ class SchoolTest < Minitest::Test
 
     assert_equal '16:00', school1.end_time
     assert_equal '12:00', school2.end_time
+  end
+
+  def test_if_school_is_full_time
+    school = School.new('9:00', 7)
+
+    assert_equal true, school.hours_in_school_day > 4
   end
 end
