e8109cbb1654391269c5efe8f6494703ec214661
David Tran <davidttemail@gmail.com>
2020-01-29 13:10:23 -0700

    Fix parameters

--- lib/school.rb
+++ lib/school.rb
@@ -1,6 +1,7 @@
 class School
-  def initialize(start_time, students)
+  def initialize(start_time, hours_in_school_day)
     @start_time = start_time
-    @students = students
+    @hours_in_school_day = hours_in_school_day
+    @students = []
   end
 end
